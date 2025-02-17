5bb100004cfe8df90d89a78f0e8949c5f3787688
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-02-17 19:02:43 -0300

    config and test mailhog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RH MANGNT";
+Route::get('/email', function () {
+    Mail::raw('Test Message', function ($message) {
+        $message->to('teste@example.com');
+    });
+
+    echo 'Email sent';
 });
