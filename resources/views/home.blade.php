e5e40c0f0b8c4d3296cb4959a43ca1b9e4292e2a
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-02-22 15:48:28 -0300

    feat: admin gate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,8 @@
 <x-layout-app page-title="Home">
     <p class="display-6 text-center my-5">Home Page</p>
+
+    @can('admin')
+        <p class="text-center">You are an admin</p>
+    @endcan
+    
 </x-layout-app>
