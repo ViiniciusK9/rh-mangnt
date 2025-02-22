0c26fdfb97f9e0a83640cfcabed69d0026554b52
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2025-02-22 08:41:49 -0300

    feat: starting guest layout

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,3 @@
+<x-layout-guest page-title="Login">
+    <p class="display-6 text-center my-5">Login Form</p>
+</x-layout-guest>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/email', function () {
-    Mail::raw('Test Message', function ($message) {
-        $message->to('teste@example.com');
-    });
-
-    echo 'Email sent';
-});
+Route::view('/login', 'auth.login')->name('login');
